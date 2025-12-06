88fa76f883ea803eafed2c06c7dccc541bbef9d6
Clinton Ochieng <clintonochieng072@gmail.com>
2025-12-06 19:22:54 +0300

    `Added status property to IProperty interface`

--- src/lib/models/Property.ts
+++ src/lib/models/Property.ts
@@ -14,6 +14,7 @@ export interface IProperty extends Document {
   bedrooms?: number;
   bathrooms?: number;
   area?: number;
+  status?: string;
   isPublished: boolean;
   paid: boolean;
   planType?: "monthly" | "one_time";
