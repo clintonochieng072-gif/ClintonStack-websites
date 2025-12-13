45531bb7e83f074f04022fdf2b5c932ba0dd3380
Clinton Ochieng <clintonochieng072@gmail.com>
2025-12-13 15:47:41 +0300

    `Update import paths to use relative path to auth module`

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { auth } from "@/auth";
+import { auth } from "../../../../auth";
 import { usersRepo } from "@/repositories/usersRepo";
 import { getUserFromToken } from "@/lib/auth";
 
