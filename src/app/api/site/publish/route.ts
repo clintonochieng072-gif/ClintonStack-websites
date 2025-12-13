6cff4e6a588df31ff7616910ac709c1f8d4d14f4
Clinton Ochieng <clintonochieng072@gmail.com>
2025-12-13 12:19:09 +0300

    `Update import of auth module to use @/lib/auth-nextauth`

--- src/app/api/site/publish/route.ts
+++ src/app/api/site/publish/route.ts
@@ -1,7 +1,7 @@
 // src/app/api/site/publish/route.ts
 
 import { Site } from "@/lib/models/Site";
-import { auth } from "@/auth";
+import { auth } from "@/lib/auth-nextauth";
 import { usersRepo } from "@/repositories/usersRepo";
 import { connectDb } from "@/lib/db";
 
